318a53f09324354239998f09be973b65ac014019
Chanpreet Sembhi <chanpreetsingh73553@gmail.com>
2025-07-23 06:58:03 +0530

    improve title styles

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1 class="text-2xl font-bold text-center md:text-start pb-6">
-        <slot />
-    </h1>
+    <div class="bg-sky-50 text-sky-600 ring-1 ring-sky-200 rounded-lg p-4 mb-6 text-center">
+        <h1 class="text-3xl font-bold pb-1">
+            Topics
+        </h1>
+        <p class="text-neutral-800 text-sm">
+            <slot />
+        </p>
+    </div>
 </template>
--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -8,11 +8,11 @@ import { RouterLink } from 'vue-router';
         <div class="mx-auto max-w-2xl">
             <div class="hidden sm:mb-8 sm:flex sm:justify-center">
             </div>
-            <div class="text-center">
-                <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            <div class="text-center p-5">
+                <h1 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
                     NEP Education Notes For Students
                 </h1>
-                <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Explore student-friendly
+                <p class="mt-5 text-base font-medium text-pretty text-gray-500 sm:text-lg/8">Explore student-friendly
                     notes designed to simplify complex topics. From English literature to NEP concepts, learn smarter
                     with quick summaries and key points.</p>
                 <div class="mt-10">
