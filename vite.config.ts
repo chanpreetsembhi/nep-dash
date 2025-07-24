5c2abaa836ec5176099563acfe886288f3f24157
Chanpreet Sembhi <chanpreetsingh73553@gmail.com>
2025-07-24 10:40:25 +0530

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://nep-api-orcin.vercel.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
 })
